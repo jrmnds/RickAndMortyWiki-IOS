92000b92c48fb7f5e026b9dc2aa4bc157e97945f
Reginaldo Mendes Junior <rmendesj@SAO-C02TN40JHV2L.local>
2021-10-07 12:00:40 -0300

    criacao do repositorio IOS

---
+++ RickAndMortyWiki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyWiki
+//
+//  Created by Reginaldo Mendes Junior on 07/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
